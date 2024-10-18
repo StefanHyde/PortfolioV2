3cbbfd5f67345522b7c72dca12728420930dd2ab
stephaneheyd <heyd.stephane@gmail.com>
2024-10-18 07:15:37 +0200

    Add colors vars and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
+import { Nunito_Sans, Montserrat } from "next/font/google";
+
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+export const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-nunito",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
@@ -24,12 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${nunito.variable} ${montserrat.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
