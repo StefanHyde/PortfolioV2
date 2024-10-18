f9bdcc4a969d0ccfd9284e94b4bb2e15fd891f4b
stephaneheyd <heyd.stephane@gmail.com>
2024-10-18 14:47:53 +0200

    Add hero component WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Hero from '../components/hero';
+
 export default function Home() {
   return (
-    <div className="h-96 bg-gradient-to-r from-primary-500 to-secondary-500 inline-block text-transparent bg-clip-text">
-      <p className="font-nunito font-semibold text-9xl">Hello world !</p>
-    </div>
+    <main>
+      <div className="fixed left-0 top-0 -z-10 h-full w-full">
+        <div className="realtive h-full w-full bg-white dark:bg-slate-950">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        </div>
+      </div>
+      <div className="relative mx-auto h-screen w-full max-w-7xl flex items-center justify-center px-6 md:px-8 lg:px-12">
+        <Hero
+          surtitle="developpé par:"
+          title="Stefan Heyd"
+          subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </main>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+type Props = {
+  surtitle?: string | null;
+  title: string;
+  subtitle?: string | null;
+};
+
+export default function Hero({ surtitle, title, subtitle }: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="relative mx-auto flex max-w-2xl flex-col items-center mt-8">
+        <div className="bg-gradient-to-r from-primary-500 to-secondary-500 inline-block text-transparent bg-clip-text">
+          <h1 className="flex flex-col font-nunito font-bold text-6xl md:text-8xl uppercase">
+            <span className="text-dark-800 dark:text-almost-white text-sm md:text-lg font-thin">
+              {surtitle}
+            </span>
+            {title}
+          </h1>
+
+          <h2 className="font-nunito text-dark-800 dark:text-almost-white font-light text-xl md:text-2xl pt-6">
+            {subtitle}
+          </h2>
+          <div className="mt-10 flex flex-wrap gap-x-6 gap-y-3 ">
+            <button className="bg-gradient-to-r from-primary-500 to-secondary-500 text-white font-nunito font-bold text-lg px-6 py-2 mt-6 rounded-lg">
+              En savoir plus
+            </button>
+            <button className="bg-gradient-to-r from-primary-500 to-secondary-500 text-white font-nunito font-bold text-lg px-6 py-2 mt-6 rounded-lg">
+              Un projet ? Venez en parler ☕️
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
