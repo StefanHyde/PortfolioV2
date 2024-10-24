2c8ff97ead4b2439db26661a61a83deaf8c4988c
stephaneheyd <heyd.stephane@gmail.com>
2024-10-24 07:59:54 +0200

    Update theme swtich position

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from './themeSwitch';
 
 export default function Header() {
   return (
-    <header className=" z-50 flex items-center justify-between py-8">
+    <header className=" z-50 flex items-center justify-end p-8">
       <div className=" bg-white dark:bg-slate-950">
         <ThemeSwitch />
       </div>
