0c765a945f69b23f008c69a3a9ec82e09fc011b2
stephaneheyd <heyd.stephane@gmail.com>
2024-10-25 15:37:49 +0200

    Add social links to data file

--- lib/data.ts
+++ lib/data.ts
@@ -20,3 +20,21 @@ export const links = [
     url: '/',
   },
 ] as const;
+
+export const socials = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/DrHyde01',
+    icon: 'github',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/stephane-heyd/',
+    icon: 'linkedin',
+  },
+  {
+    name: 'Twitter',
+    url: 'https://x.com/DrHyde01',
+    icon: 'twitter',
+  },
+] as const;
