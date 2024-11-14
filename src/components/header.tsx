72c4764c59f27220d63596f0afd66a83f929c8af
stephaneheyd <heyd.stephane@gmail.com>
2024-11-14 11:15:53 +0100

    Update dropdown menu behaviour

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,12 +6,13 @@ import { useState, useEffect } from 'react';
 import ThemeSwitch from './themeSwitch';
 import { links } from '../../lib/data';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { HiBars3 } from 'react-icons/hi2';
+import { HiBars3, HiMiniXMark } from 'react-icons/hi2';
 
 export default function Header() {
   const pathname = usePathname();
   const isHomePage = pathname === '/';
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
 
   useEffect(() => {
     //Check if the user has scrolled to change the header background
@@ -28,38 +29,18 @@ export default function Header() {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpened((prevState) => !prevState);
+  };
+
   return (
     <header
       className={`fixed z-50 h-auto w-full flex items-center justify-between py-2 px-4 ${scrolled ? 'bg-almost-white dark:bg-slate-950' : 'bg-transparent'}`}
     >
-      <Link href="/" className="hidden md:flex">
+      <Link href="/" className="flex">
         <Image src="/images/logo.png" alt="logo" width={40} height={40} />
       </Link>
       <nav className="flex">
-        <div className="mobile-menu    md:hidden">
-          <Menu>
-            <MenuButton className="inline-flex items-center">
-              <HiBars3 className=" h-10 w-10 text-dark-800 dark:text-almost-white" />
-            </MenuButton>
-            <MenuItems
-              transition
-              anchor="bottom end"
-              className=" z-50 w-72  h-auto   [--anchor-gap:16px] backdrop-blur-md p-8 ml-6 border-solid border-2 border-primary-500 dark:border-almost-white rounded-md  flex flex-col justify-center gap-10  transition duration-300 ease-in-out focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
-            >
-              {links.map((link) => (
-                <MenuItem key={link.hash}>
-                  <Link
-                    href={link.url}
-                    className="text-lg font-nunito text-primary-500 dark:text-almost-white hover:text-primary-800 dark:hover:text-primary-800 ease-in-out duration-300"
-                  >
-                    {link.name}
-                  </Link>
-                </MenuItem>
-              ))}
-            </MenuItems>
-          </Menu>
-        </div>
-
         <ul className="hidden md:flex gap-16">
           {links.map((link) => (
             <li key={link.hash}>
@@ -80,11 +61,50 @@ export default function Header() {
 
       <div className="flex items-center gap-6">
         <ThemeSwitch />
-        <Link href="/">
+        <Link href="/" className="hidden md:flex">
           <button className="flex bg-primary-500 dark:bg-primary-800 hover:bg-primary-800 dark:hover:bg-primary-900 to-secondary-500 border-solid border-2 border-primary-500 dark:border-primary-800 hover:border-primary-800 dark:hover:border-primary-900 text-white font-montserrat font-light text-sm text-left p-2  rounded-md ease-in-out duration-300">
             Me contacter
           </button>
         </Link>
+        <div className="mobile-menu flex items-center md:hidden">
+          <Menu>
+            <MenuButton
+              className="inline-flex items-center ml-6"
+              onClick={toggleMenu}
+            >
+              {!menuOpened ? (
+                <HiBars3 className=" h-10 w-10 text-dark-800 dark:text-almost-white" />
+              ) : (
+                <HiMiniXMark className=" h-10 w-10 text-dark-800 dark:text-almost-white" />
+              )}
+            </MenuButton>
+            <MenuItems
+              transition
+              anchor="top start"
+              className=" z-50 w-full h-screen p-8 [--anchor-gap:8px] backdrop-blur-xl border-solid border-2 border-primary-500 dark:border-almost-white rounded-md  flex flex-col justify-center gap-10  transition duration-300 ease-in-out focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+            >
+              {links.map((link) => (
+                <MenuItem key={link.hash}>
+                  <Link
+                    href={link.url}
+                    onClick={() => setMenuOpened(false)}
+                    className="text-lg font-nunito text-primary-500 dark:text-almost-white hover:text-primary-800 dark:hover:text-primary-800 ease-in-out duration-300"
+                  >
+                    {link.name}
+                  </Link>
+                </MenuItem>
+              ))}
+              <MenuItem>
+                <button
+                  className="flex justify-center bg-primary-500 dark:bg-primary-800 text-lg font-nunito text-almost-white border-solid border-2 border-primary-500 dark:border-primary-800 hover:border-primary-800 dark:hover:border-primary-900 p-2 mt-12 rounded-md "
+                  onClick={() => setMenuOpened(false)}
+                >
+                  Me contacter
+                </button>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
       </div>
     </header>
   );
