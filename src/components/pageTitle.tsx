b514ac1c13a5dd557dc0a53c4f7601af75605596
stephaneheyd <heyd.stephane@gmail.com>
2025-01-09 12:00:39 +0100

    Add title size prop to pageTitle component

--- src/components/pageTitle.tsx
+++ src/components/pageTitle.tsx
@@ -5,16 +5,29 @@ import { motion } from 'framer-motion';
 type pageTitleProps = {
   title: string;
   subtitle?: string | null;
+  titleSize?: 'normal' | 'small';
 };
 
-export default function PageTitle({ title, subtitle }: pageTitleProps) {
+export default function PageTitle({
+  title,
+  subtitle,
+  titleSize,
+}: pageTitleProps) {
   const titleAnimationText = title
     .split('')
     .map((char) => (char === ' ' ? '\u00A0' : char));
+
+  const titleSizeClass =
+    titleSize === 'small'
+      ? 'text-3xl md:text-5xl uppercase'
+      : 'text-6xl md:text-7xl uppercase';
+
   return (
-    <div className="relative mx-auto h-auto w-full max-w-7xl flex flex-col items-start px-6 mt-32">
+    <div className="relative mx-auto h-auto w-full md:max-w-7xl flex flex-col items-start px-6 mt-32">
       <div className="bg-gradient-to-r from-primary-500 to-secondary-500 inline-block text-transparent bg-clip-text">
-        <div className="flex flex-col font-nunito font-bold text-6xl md:text-7xl  uppercase">
+        <div
+          className={`flex flex-col font-nunito font-bold  ${titleSizeClass}  `}
+        >
           <div className="flex flex-row flex-wrap">
             {titleAnimationText.map((el, i) => (
               <motion.h1
