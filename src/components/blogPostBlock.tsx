942c0c6179952505bb8b58fa2fabf7fa9e54f101
stephaneheyd <heyd.stephane@gmail.com>
2025-01-22 09:08:16 +0100

    Update blog posts list styles

--- src/components/blogPostBlock.tsx
+++ src/components/blogPostBlock.tsx
@@ -1,7 +1,6 @@
+import defaultPostImg from '@images/defaultblog.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
-
-import defaultPostImg from '@images/defaultblog.jpg';
 
 interface Post {
   id: string;
@@ -23,9 +22,9 @@ interface Post {
 export default function BlogPostBlock({ post }: { post: Post }) {
   return (
     <Link href={`/blog/posts/${post.slug}`}>
-      <div
+      <article
         key={post.id}
-        className='relative overflow-hidden  pt-0  px-0  row-span-3 flex flex-col h-96 backdrop-blur-md   border-solid border-2 border-primary-500 dark:border-almost-white rounded-md bg-gradient-to-br from-primary-50 dark:from-primary-950 to-transparent shadow-md"'
+        className="group relative overflow-hidden  pt-0  px-0 row-span-3 flex flex-col h-72 md:h-96 backdrop-blur-md   border-solid border-2 border-primary-500 dark:border-almost-white rounded-md bg-gradient-to-br from-primary-50 dark:from-primary-950 to-transparent shadow-md hover:shadow-lg ease-in-out duration-300"
       >
         {post.featuredImage && post.featuredImage.node.sourceUrl ? (
           <Image
@@ -33,7 +32,7 @@ export default function BlogPostBlock({ post }: { post: Post }) {
             alt={post.title}
             width={500}
             height={300}
-            className="w-full h-32 md:h-48 object-cover "
+            className="w-full h-1/2 object-cover group-hover:brightness-90 ease-in-out duration-300"
           />
         ) : (
           <Image
@@ -41,22 +40,25 @@ export default function BlogPostBlock({ post }: { post: Post }) {
             alt="image par défaut"
             width={500}
             height={300}
-            className="w-full h-32 md:h-48 object-cover "
+            className="w-full h-1/2"
           />
         )}
-        <div className="flex flex-col h-full p-4">
-          <h3 className="flex-1 text-xl font-montserrat font-semibold group-hover:text-primary-500 ease-in-out duration-300">
+        <div className="flex flex-col p-4 h-1/2 justify-around">
+          <h3 className="flex-1 text-base md:text-lg  font-montserrat font-semibold text-ellipsis overflow-hidden group-hover:text-primary-500 ease-in-out duration-300">
             {post.title}
           </h3>
           <div className="flex-none text-sm text-gray-500 mt-3">
             {post.tags.nodes.map((tag: { name: string }) => (
-              <span key={tag.name} className="mr-2">
+              <span
+                key={tag.name}
+                className="mr-2 p-1 text-xs bg-primary-200 dark:bg-primary-500 text-primary-900 dark:text-almost-white rounded-sm "
+              >
                 {tag.name}
               </span>
             ))}
           </div>
         </div>
-      </div>
+      </article>
     </Link>
   );
 }
