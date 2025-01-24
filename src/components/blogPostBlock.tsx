ea2054b412f25304ff5b033c034418f577e8463e
stephaneheyd <heyd.stephane@gmail.com>
2025-01-24 09:48:20 +0100

    Improve hover on blog posts cards

--- src/components/blogPostBlock.tsx
+++ src/components/blogPostBlock.tsx
@@ -27,21 +27,25 @@ export default function BlogPostBlock({ post }: { post: Post }) {
         className="group relative overflow-hidden  pt-0  px-0 row-span-3 flex flex-col h-72 md:h-96 backdrop-blur-md   border-solid border-2 border-primary-500 dark:border-almost-white rounded-md bg-gradient-to-br from-primary-50 dark:from-primary-950 to-transparent shadow-md hover:shadow-lg ease-in-out duration-300"
       >
         {post.featuredImage && post.featuredImage.node.sourceUrl ? (
-          <Image
-            src={post.featuredImage.node.sourceUrl}
-            alt={post.title}
-            width={500}
-            height={300}
-            className="w-full h-1/2 object-cover group-hover:brightness-90 ease-in-out duration-300"
-          />
+          <div className="relative w-full h-1/2 overflow-hidden">
+            <Image
+              src={post.featuredImage.node.sourceUrl}
+              alt={post.title}
+              width={500}
+              height={300}
+              className="w-full h-full object-cover group-hover:brightness-90 group-hover:scale-105 ease-in-out duration-300"
+            />
+          </div>
         ) : (
-          <Image
-            src={defaultPostImg}
-            alt="image par défaut"
-            width={500}
-            height={300}
-            className="w-full h-1/2"
-          />
+          <div className="relative w-full h-1/2 overflow-hidden">
+            <Image
+              src={defaultPostImg}
+              alt="image par défaut"
+              width={500}
+              height={300}
+              className="w-full h-full group-hover:brightness-90 group-hover:scale-105 ease-in-out duration-300"
+            />
+          </div>
         )}
         <div className="flex flex-col p-4 h-1/2 justify-around">
           <h3 className="flex-1 text-base md:text-lg  font-montserrat font-semibold text-ellipsis overflow-hidden group-hover:text-primary-500 ease-in-out duration-300">
