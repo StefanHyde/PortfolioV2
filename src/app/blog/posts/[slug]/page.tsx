0660e0b4488c59311a6c3df8a608eaed1b320b95
stephaneheyd <heyd.stephane@gmail.com>
2025-01-29 09:00:50 +0100

    Minor improve on blog posts

--- src/app/blog/posts/[slug]/page.tsx
+++ src/app/blog/posts/[slug]/page.tsx
@@ -3,11 +3,12 @@ import { getPostBySlug } from '@api/wordPress/service';
 import BlogPostHeader from '@/components/blogPostHeader';
 
 export default async function BlogPost({
-  params,
+  params: paramsPromise,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
-  const post = await getPostBySlug(params.slug);
+  const { slug } = await paramsPromise;
+  const post = await getPostBySlug(slug);
 
   return (
     <main>
@@ -17,10 +18,10 @@ export default async function BlogPost({
           coverImage={post.featuredImage?.node?.sourceUrl || null}
         />
       </div>
-      <div className="text-dark-800 dark:text-almost-white mx-auto mt-24 mb-12 flex w-full max-w-7xl flex-col justify-center gap-10 p-6 md:p-12 xl:mt-32">
+      <div className="text-dark-800 dark:text-almost-white mx-auto flex w-full flex-col justify-center gap-10 p-6 md:p-12">
         <section>
           <div
-            className="post-content mx-auto mt-20 w-full py-6 text-lg md:w-3/5"
+            className="post-content mx-auto w-full space-y-6 py-6 text-base sm:w-4/5 xl:w-3/5"
             dangerouslySetInnerHTML={{ __html: post.content }}
           ></div>
         </section>
--- src/components/blogPostHeader.tsx
+++ src/components/blogPostHeader.tsx
@@ -14,7 +14,7 @@ export default function BlogPostHeader({
   return (
     <div className="relative mx-auto mt-14 flex h-auto w-full flex-col items-start">
       <div className="from-dark-950 absolute top-0 left-0 z-10 h-full w-full bg-gradient-to-t from-5% to-transparent dark:from-slate-950"></div>
-      <div className="absolute z-20 flex h-full w-full items-end justify-center p-4 md:p-12">
+      <div className="absolute z-20 flex h-full w-full items-end justify-center p-4 md:justify-start md:p-12">
         <h1 className="font-montserrat text-almost-white mb-6 text-4xl font-semibold md:text-5xl">
           {title}
         </h1>
