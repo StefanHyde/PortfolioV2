ff4e29ae51c542c8168aff02ba587c36418e5726
stephaneheyd <heyd.stephane@gmail.com>
2025-03-24 11:58:51 +0100

    Add packages for mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import createMDX from '@next/mdx';
 
 const withNextIntl = createNextIntlPlugin();
+const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     images: {
         domains: ['blog.stefanheyd.com'],
     },
 };
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(withMDX(nextConfig));
