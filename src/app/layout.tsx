1a857c67c1a47a5c04ca9906dc16e3686cb08eab
stephaneheyd <heyd.stephane@gmail.com>
2025-07-14 16:42:31 +0200

    Update Next.js version and adjust image configuration in next.config.mjs; update package-lock.json for various dependencies including sharp and detect-libc.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
           <Header />
           <div className="fixed top-0 left-0 -z-10 h-full w-full">
             <div className="realtive h-full w-full bg-white dark:bg-slate-950">
-              <div className="absolute top-0 right-0 bottom-0 left-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] bg-[size:14px_24px]"></div>
+              <div className="absolute top-0 right-0 bottom-0 left-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
             </div>
           </div>
           <ScrollToTop />
